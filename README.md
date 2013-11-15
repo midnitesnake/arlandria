cf1fdcebfce6b7fcdf75f54050106b6e3ed52386
kussic <kussic@Hackbook-Air.local>
2013-11-15 20:07:18 +0000

    Init
    Actual code uploaded, plus some other minor stuff.

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-arlandria
-=========
+arlandria.py
+============
 
 Simple LinkedIn scrapper for OSINT
+
+# What?
+A very simple python script that uses the Google Custom Search API to search for people on LinedIn based on a query or a Google dork.
+It takes the query and spits out a CSV formatted output.
+Mostly useful for creating phishing campaigns.
+
+#Why?
+It's mid-2012 or so, you boot your laptop and realise that this week's special is yet another phishing job, which means yet another full day spent trying to identify "targets". 
+
+If this sounds familiar so far, you know what's next. After the first 2 or so jobs you've realised that it's quite repetitive, meandering and borderline moronic and some tooling is in order. In my case, having tried a few other similar tools but not being satisfied, it was time to throw in the gauntlet and try my own luck.
+
+What I found most useful was the ability to generate a CSV file that includes not only **names** and **emails** but also **location**, and **job role**. It made my life a lot easier identifying individuals for spear phishing campaigns and it also produces something more presentable for the report.
+
+[tl;dr] A proof-of-concept toy that does something sort of useful but my coding sucks and hopefully someone will fork it!  
+
+#How?
+You need the Google API client and a key:
+* "easy_install --upgrade google-api-python-client"
+* Get a Google API Developer Key ([here][gkey])
+* Put the key in a file called *"google_api.key"* on the same location as the code.
+
+	SEARCH PARAMETERS:
+	  
+	 -q | --query    : Specify the query (Usually the company name)  
+	 -f | --format   : Specify the email format   
+	 -m | --max      : Maximum results to return  [Default: 10]
+	 -d | --dork     : Query is actually a dork   [Default: False]
+
+	DEBUG PARAMETERS:
+	 -j | --json     : Spit out the JSON          [Default: False]
+	 
+	 
+	FORMAT TEMPLATE:
+	{fn} = First Name
+	{sn} = Surname 
+	{fi} = First Initial
+	{si} = Surname Initial
+
+
+	EXAMPLES:
+	 Standard Query:
+	 * arlandria.py -q 'BobCorp' -f '{fn}.{sn}@BobCorp.com'
+
+	 Dork Query:
+	 * arlandria.py -d -q 'site:uk.linkedin.com/pub/ "current  "at BobCorp LLP"' -f '{sn}{fi}@BobCorp.com'
+
+
+[gkey]: http://code.google.com/apis/console
---
+++ arlandria.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Arlandria - Simple LinkedIn scrapper for OSINT
+# Copyright (C) 2013  kussic@chaos6.net (Ari Davies)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# You need the Google API, to install:
+#  1) "easy_install --upgrade google-api-python-client"
+#  2) Get a Google API Developer Key (http://code.google.com/apis/console)
+#  3) Put the key in a file called "google_api.key" on the same location as the code.
+
+__author__ = 'kussic@chaos6.net (Ari Davies)'
+__version__ = '0.1'
+
+import os, sys, getopt,pprint,string
+from apiclient.discovery import build
+
+def logo():
+
+	print '\n\nArlandria - v' + __version__
+
+def cusage():
+
+	logo()
+	print """
+
+SEARCH PARAMETERS:
+  
+ -q | --query    : Specify the query (Usually the company name)  
+ -f | --format   : Specify the email format   
+ -m | --max      : Maximum results to return  [Default: 10]
+ -d | --dork     : Query is actually a dork   [Default: False]
+
+DEBUG PARAMETERS:
+ -j | --json     : Spit out the JSON          [Default: False]
+ 
+ 
+FORMAT TEMPLATE:
+{fn} = First Name
+{sn} = Surname 
+{fi} = First Initial
+{si} = Surname Initial
+
+
+EXAMPLES:
+ Standard Query:
+ * arlandria.py -q 'BobCorp' -f '{fn}.{sn}@BobCorp.com'
+
+ Dork Query:
+ * arlandria.py -d -q 'site:uk.linkedin.com/pub/ "current  "at BobCorp LLP"' -f '{sn}{fi}@BobCorp.com'
+ 
+ """
+
+def harvest(query,hcard,page):
+
+  devKeyfile = open('google_api.key', 'r')
+  devKey = devKeyfile.readline().rstrip()
+  devKeyfile.close()
+
+  service = build("customsearch", "v1", developerKey=devKey)
+   
+  #We have the option of also using 'hcard' simply because it's cleaner, nontheless
+  #not all google results have 'hcard', thus we use 'title'
+
+  # -- NOT FULLY IMPLEMENTED YET --
+  
+  if hcard == True:
+   fieldsQ = 'items/pagemap/hcard(fn,title)'
+  else:
+   fieldsQ = 'items/title,items/snippet,items/pagemap/person(role,location)'
+
+  if dork == True:
+   #sys.stderr.write('Using Open CSE\n')
+   cxQ = '013036536707430787589:_pqjad5hr1a' #Open CSE
+  else:
+   #sys.stderr.write('Using default CSE\n')
+   cxQ = '016629816658822411423:xzvfv-aza9y' #*.linkedin.com/pub CSE
+
+   
+
+  global res
+
+  res = service.cse().list(
+      prettyPrint='false',
+      q=query,
+      cx=cxQ,
+      fields=fieldsQ,
+      start=page,
+    ).execute()
+
+  if json == True:
+   print "-------- <JSON> ----------"
+   pprint.pprint(res)
+   print "-------- </JSON> ----------"
+   
+ 
+  itemsStr = res['items']
+
+  for i in itemsStr:
+   personName = i['title']
+   personSnippet = i['snippet']
+   print '' 
+   s = string.Template(formatStr)
+   fname = string.split(personName)[0].lower()
+   lname = string.split(personName)[1].lower()
+   finit = ''.join(map(lambda w: w[:1], fname.split()))
+   linit = ''.join(map(lambda w: w[:1], lname.split()))
+   sys.stdout.write(s.substitute(fn=fname, sn=lname, fi=finit, si=linit))
+       
+   
+   sys.stdout.write(',' + fname + ' ' + lname) 
+   try:
+    pagemap = i['pagemap']
+    personCard = pagemap['person']
+    for x in personCard:
+     sys.stdout.write(',"' + x['role'] + '"')
+     sys.stdout.write(',"' + x['location'] + '"')
+   except:
+    continue	
+	
+def main():
+
+  try:
+   if sys.argv[1] :
+    opts, args = getopt.getopt(sys.argv[1:], "q:jf:m:d", ["query=", "json", "format=", "max=", "dork"])
+    #print "Opts: " + str(opts) + " Args: " + str(args)
+
+    #defaults go here
+    global json
+    json = False
+    global filter
+    format = False
+    page = 10
+    global formatStr
+    global dork
+    dork = False
+    
+    
+
+
+    for o, a in opts:
+     if o == "-q" or o == "--query":
+      query = a
+     if o == "-j" or o == "--json":
+      json = True
+     # Mith -- added format parameter 
+    
+     if o == "-f" or o == "--format":
+      format = True
+      formatStr = a.replace('{','${')
+    
+     if o == "-m" or o == "--max":
+      page = int(a)
+ 
+     if o == "-d" or o == "--dork":
+      dork = True 
+
+  except getopt.GetoptError:
+   cusage()
+   sys.exit(1)
+  except IndexError:
+   cusage()
+   sys.exit(1) 
+
+  #Check that the user has specified a --format
+  if format == False:
+   sys.stderr.write('Error: No --format specified...')
+   cusage()
+   sys.exit(1)   
+
+  #Initiating the harvest loop
+  sys.stdout.write('Email,' + 'Name,' + 'Role,' + 'Location')  
+  try: 
+   for pagenum in range(1,page,10):
+    #sys.stderr.write('\nGoogle Results - Results ' + str(pagenum) + ' to ' + str(pagenum+9) + "\n")
+    harvest(query,False,pagenum)
+  except:
+   print "Error: ", sys.exc_info()[1]
+   sys.exit(1)
+
+  print ''
+
+
+if __name__ == '__main__':
+  main()
